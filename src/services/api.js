aad809f7896132d4e1eb73f6e43f29c2794cf9ad
AITuring <shspku365@gmail.com>
2020-11-23 19:34:30 +0800

    改了小bug，但是现在网络有问题，是之前添加历史上今天接口出现的问题，需要修复😅

--- src/services/api.js
+++ src/services/api.js
@@ -1,8 +1,8 @@
 //跨域代理前缀
-// const API_PROXY_PREFIX='/today'
+const API_PROXY_PREFIX='https://api.xhboke.com/today/v1'
 const TODAY_URL = 'https://api.xhboke.com/'
 const BASE_URL = process.env.NODE_ENV === 'production' ? process.env.VUE_APP_API_BASE_URL : API_PROXY_PREFIX
-const BASE_URL = process.env.VUE_APP_API_BASE_URL
+// const BASE_URL = process.env.VUE_APP_API_BASE_URL
 module.exports = {
   LOGIN: `${BASE_URL}/login`,
   ROUTES: `${BASE_URL}/routes`,
